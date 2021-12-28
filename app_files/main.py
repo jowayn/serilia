efb1abd5a929f7ef8bb31932a39e6d21a472eaaf
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2021-12-28 15:07:54 +0800

    Update main.py

--- app_files/main.py
+++ app_files/main.py
@@ -5,7 +5,6 @@ import pandas as pd
 from st_aggrid import AgGrid, DataReturnMode, GridUpdateMode, GridOptionsBuilder, JsCode
 
 st.header("Serilia - Update IP Masterlist")
-st.subheader("by jowayn")
 
 df_template = pd.DataFrame(
     '',
@@ -28,7 +27,7 @@ def get_data_ex7():
 
 data = get_data_ex7()
 
-st.subheader("Edit Data")
+st.subheader("Edit Database")
 gb = GridOptionsBuilder.from_dataframe(data)
 #make all columns editable
 gb.configure_columns(['srcip','sport','dstip','dsport','sttl','dttl','tcprtt','synack','ct_state_ttl','Label'], editable=True)
